99abfcf51aedf91e8c3181136b8f98bfa61615b3
amunishkin <munishkinalex@gmail.com>
2025-07-24 21:57:26 -0700

    Conway's game of life sim

---
+++ udemy/Adv_Modern_Cpp/e1/main.cpp
@@ -0,0 +1,42 @@
+#include "grid.h"
+
+// Uncoment if running in Windows Console
+//#include "ansi_escape.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Conway's game of Life\n";
+    std::cout << "Press the return key to display each generation\n";
+
+    // Wait for user to press the return key
+    std::cin.get();
+
+    // Uncomment if running in Windows Console
+    // Enable ANSI escape codes on Windows
+    //setupConsole();
+
+    // Grid for the first generation
+    grid current_generation;
+
+    // Populate the cells at random
+    current_generation.randomize();
+
+    while (true) {
+        // Draw the current generation
+        current_generation.draw();
+
+        // Wait for user to press the return key
+        std::cin.get();
+
+        // Grid for the next generation
+        grid next_generation;
+
+        // Populate the cells in the next generation
+        calculate(current_generation, next_generation);
+
+        // Update to the next generation
+        current_generation.update(next_generation);
+    }
+
+    return 0;
+}
