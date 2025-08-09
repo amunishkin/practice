c9d7e9ec309fa98743886d92059e90dc16fa3f72
amunishkin <munishkinalex@gmail.com>
2025-08-09 12:46:08 -0700

    finished up to lec27

---
+++ udemy/Graphics_Vulkan_Cpp/Windows/src/glfw_initialization.cpp
@@ -0,0 +1,16 @@
+#include <glfw_initialization.h>
+#include <GLFW/glfw3.h>
+
+namespace veng {
+
+GlfwInitialization::GlfwInitialization() {
+  if (glfwInit() != GLFW_TRUE) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+GlfwInitialization::~GlfwInitialization() {
+  glfwTerminate();
+}
+
+}  // namespace veng
---
+++ udemy/Graphics_Vulkan_Cpp/Windows/src/main.cpp
@@ -0,0 +1,23 @@
+#include <GLFW/glfw3.h>
+#include <glfw_initialization.h>
+#include <glfw_monitor.h>
+
+std::int32_t main(std::int32_t argc, gsl::zstring* argv) {
+  const veng::GlfwInitialization _glfw;
+
+  gsl::not_null window = glfwCreateWindow(800, 600, "Vulkan Engine", nullptr, nullptr);
+  gsl::final_action _cleanup_window([window]() {
+    glfwDestroyWindow(window);
+  });
+
+  gsl::span<GLFWmonitor*> monitors = veng::GetMonitors();
+  if (monitors.size() > 1) {
+    veng::MoveWindowToMonitor(window, monitors[1]);
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  return EXIT_SUCCESS;
+}
