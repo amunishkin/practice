0598ea012caeadda85eddb0bf1ca931f0761d8f3
Munishkin <amunishk@ndc.nasa.gov>
2025-08-11 07:37:32 -0700

    udemy java masterclass HelloWorld

---
+++ udemy/Masterclass_Java/Pratice/src/experimenting/HelloWorld.java
@@ -0,0 +1,13 @@
+package experimenting;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		int my_first_number = 5;
+		System.out.println(Integer.toString(my_first_number));
+		my_first_number = 10;
+		System.out.println(my_first_number);
+		
+		//StringBuilder
+	}
+}
